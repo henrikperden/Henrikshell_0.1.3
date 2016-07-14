f554f5cdd55dd60b00c6ca1e208fe2a467ae3ae7
henrikperden <henrik.perden@gmail.com>
2016-07-14 22:28:29 +0200

    Sulfur science

--- prototypes/science/labs.lua
+++ prototypes/science/labs.lua
@@ -257,3 +257,93 @@ data:extend(
   },
   }
   )
+  --Sulfur Lab--  
+data:extend(
+{
+ {
+    type = "item",
+    name = "sulfur-lab",
+    icon = "__base__/graphics/icons/lab.png",
+    flags = {"goes-to-quickbar"},
+    subgroup = "labs",
+    order = "a-3",
+    place_result = "sulfur-lab",
+    stack_size = 10
+  },
+ {
+    type = "recipe",
+    name = "sulfur-lab",
+    energy_required = 5,
+    enabled=true,
+    ingredients =
+    {
+      {"basic-circuit-board", 10},
+      {"iron-gear-wheel", 10},
+      {"plumbing",10},
+      {"wall", 10},
+    },
+    result = "sulfur-lab"
+  },
+  {
+    type = "lab",
+    name = "sulfur-lab",
+    icon = "__base__/graphics/icons/lab.png",
+    flags = {"placeable-player", "player-creation"},
+    minable = {mining_time = 1, result = "sulfur-lab"},
+    max_health = 150,
+    corpse = "big-remnants",
+    dying_explosion = "medium-explosion",
+    collision_box = {{-1.2, -1.2}, {1.2, 1.2}},
+    selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
+    light = {intensity = 0.75, size = 8},
+    on_animation =
+    {
+      filename = "__base__/graphics/entity/lab/lab.png",
+      width = 113,
+      height = 91,
+      frame_count = 33,
+      line_length = 11,
+      animation_speed = 1 / 3,
+      shift = {0.2, 0.15}
+    },
+    off_animation =
+    {
+      filename = "__base__/graphics/entity/lab/lab.png",
+      width = 113,
+      height = 91,
+      frame_count = 1,
+      shift = {0.2, 0.15}
+    },
+    working_sound =
+    {
+      sound =
+      {
+        filename = "__base__/sound/lab.ogg",
+        volume = 0.7
+      },
+      apparent_volume = 1
+    },
+    energy_source =
+    {
+      type = "electric",
+      usage_priority = "secondary-input"
+    },
+    energy_usage = "26MW",
+    researching_speed = 1,
+    inputs =
+    {
+      "science-pack-sulfuric-acid1",
+      "science-pack-sulfuric-acid2",
+      "science-pack-sulfuric-acid3",
+      "science-pack-sulfuric-acid4",
+    },
+    module_specification =
+    {
+      module_slots = 2,
+      max_entity_info_module_icons_per_row = 3,
+      max_entity_info_module_icon_rows = 1,
+      module_info_icon_shift = {0, 0.9}
+    }
+  },
+  }
+  )
--- prototypes/technology/sulfuric-acidscience/science-pots-sulfuric-acid.lua
+++ prototypes/technology/sulfuric-acidscience/science-pots-sulfuric-acid.lua
@@ -12,6 +12,39 @@ data:extend(
     durability_description_key = "description.science-pack-remaining-amount"
   },
   {
+    type = "tool",
+    name = "science-pack-sulfuric-acid2",
+    icon = "__base__/graphics/icons/fluid/sulfuric-acid.png",
+    flags = {"goes-to-main-inventory"},
+    subgroup = "pots",
+    order = "a[science-pack-sulfuric-acid1]",
+    stack_size = 200,
+    durability = 1,
+    durability_description_key = "description.science-pack-remaining-amount"
+  },
+  {
+    type = "tool",
+    name = "science-pack-sulfuric-acid3",
+    icon = "__base__/graphics/icons/fluid/sulfuric-acid.png",
+    flags = {"goes-to-main-inventory"},
+    subgroup = "pots",
+    order = "a[science-pack-sulfuric-acid1]",
+    stack_size = 200,
+    durability = 1,
+    durability_description_key = "description.science-pack-remaining-amount"
+  },
+  {
+    type = "tool",
+    name = "science-pack-sulfuric-acid4",
+    icon = "__base__/graphics/icons/fluid/sulfuric-acid.png",
+    flags = {"goes-to-main-inventory"},
+    subgroup = "pots",
+    order = "a[science-pack-sulfuric-acid1]",
+    stack_size = 200,
+    durability = 1,
+    durability_description_key = "description.science-pack-remaining-amount"
+  },
+  {
     type = "recipe",
     name = "science-pack-sulfuric-acid1",
     energy_required = 5,
@@ -20,12 +53,51 @@ data:extend(
     {
       {"sulfur",100},
       {"basic-circuit-board",1},
-	  {"iron-gear-wheel",10},
+      {"iron-gear-wheel",10},
     },
     result = "science-pack-sulfuric-acid1"
   },
- 
-  
-  
+ {
+    type = "recipe",
+    name = "science-pack-sulfuric-acid2",
+    energy_required = 5,
+    category="crafting",
+    ingredients =
+    {
+      {"sulfur",100},
+      {"salpeter",10},
+      {"basic-circuit-board",1},
+      {"iron-gear-wheel",10},
+    },
+    result = "science-pack-sulfuric-acid2"
+  },
+  {
+    type = "recipe",
+    name = "science-pack-sulfuric-acid3",
+    energy_required = 5,
+    category="crafting",
+    ingredients =
+    {
+      {"sulfur",100},
+      {"lead-plate",10},
+      {"basic-circuit-board",1},
+      {"iron-gear-wheel",10},
+    },
+    result = "science-pack-sulfuric-acid3"
+  },
+  {
+    type = "recipe",
+    name = "science-pack-sulfuric-acid4",
+    energy_required = 5,
+    category="crafting",
+    ingredients =
+    {
+      {"sulfur",100},
+      {"catalyst",1},
+      {"basic-circuit-board",1},
+      {"iron-gear-wheel",10},
+    },
+    result = "science-pack-sulfuric-acid4"
+  },
   }
   )
